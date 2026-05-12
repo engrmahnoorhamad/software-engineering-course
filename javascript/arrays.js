585dc16d0b6edf92761bb46031155ddd6d4ba834
Mahnoor Hamad <mahnoorhamad541@gmail.com>
2026-05-13 00:35:23 +0500

    test green mark contribution

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -7,3 +7,14 @@ console.log(fruits[0]); // Output: Apple
 console.log(fruits.length); // Output: 3
 
 // 2nd
+// // Traditional anonymous function
+// (function (a, b) {
+//   const chuck = 42;
+//   return a + b + chuck;
+// });
+
+// // Arrow function
+// (a, b) => {
+//   const chuck = 42;
+//   return a + b + chuck;
+// };
