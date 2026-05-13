634119a79e91ac791cf76ff4b36deb98072e7941
Mahnoor Hamad <mahnoorhamad541@gmail.com>
2026-05-14 00:03:36 +0500

    functions.js file update

--- javascript/functions.js
+++ javascript/functions.js
@@ -2,3 +2,12 @@ const add = (a, b) => a + b;
 
 console.log(add(5, 10)); // Output: 15
 
+function myFunc(theArr) {
+  theArr[0] = 30;
+}
+
+const arr = [45];
+
+console.log(arr[0]); // 45
+myFunc(arr);
+console.log(arr[0]); // 30
